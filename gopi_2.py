e1505d7044cde478c2d3f3e87d58c88f7620cdce
ZJC826 <gopi.kancharla@capitalone.com>
2017-04-27 16:37:33 -0500

    Disabling the log

--- gopi_2.py
+++ gopi_2.py
@@ -26,14 +26,13 @@ def checkStatus():
 		if (time.time() - START_TIME) > CHECK_TIME:
 			response = requests.get("http://buildstatuscapone.herokuapp.com/status")
 			RESPONSE_STATUS = response.text
-			print "Response Recieved: ", RESPONSE_STATUS, "Current Time: ", (time.time())
+			print "Response Recieved: ", RESPONSE_STATUS, "Current Time: ", time.strftime("%c")
 			START_TIME = time.time()
 			CHECK_TIME = 60
 
 sys.stdout = open("stdout.txt", "w")
 def setLights(pin, brightness):
-	text_file.write("PIN %s %s" % pin % brightness)
-	print "Pin: ", pin, " Brightness: ", brightness
+	#print "Pin: ", pin, " Brightness: ", brightness
 	pi.set_PWM_dutycycle(pin, brightness)
 
 def showBuildProgress():
@@ -42,13 +41,13 @@ def showBuildProgress():
 		setLights(GREEN_PIN, x)
 		setLights(BLUE_PIN, x)
 		setLights(RED_PIN, 0)
-		print "Showing IN_PROGRESS with brightness", x
+		#print "Showing IN_PROGRESS with brightness", x
 	for x in range (255, 1, -1):
 		time.sleep(0.1)
 		setLights(GREEN_PIN, x)
 		setLights(BLUE_PIN, x)
 		setLights(RED_PIN, 0)
-		print "Showing IN_PROGRESS with brightness", x
+		#print "Showing IN_PROGRESS with brightness", x
 
 
 def showBuildSuccess():
@@ -62,13 +61,13 @@ def showBuildFailure():
 		setLights(GREEN_PIN, 0)
 		setLights(BLUE_PIN, x)
 		setLights(RED_PIN, x)
-		print "Showing FAIL with brightness", x
+		#print "Showing FAIL with brightness", x
 	for x in range (255, 1, -1):
 		time.sleep(0.2)
 		setLights(GREEN_PIN, 0)
 		setLights(BLUE_PIN, x)
 		setLights(RED_PIN, x)
-		print "Showing FAIL with brightness", x
+		#print "Showing FAIL with brightness", x
 
 def turnOff():
 	setLights(GREEN_PIN,0)
